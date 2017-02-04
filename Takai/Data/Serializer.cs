4334eded6705b5db1f7cc21be1c2f0bf3480ef4a
cobalthex <cobalthex@live.com>
2017-02-03 18:18:43 -0800

    Tweaking custom serialization to support adding additional fields rather than custom serializers

--- Takai/Data/Serializer.cs
+++ Takai/Data/Serializer.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Text;
-using System.Linq;
-using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.Xna.Framework;
