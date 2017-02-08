ff7b6aae3140de2605e86015c0aafb0ca313a649
cobalthex <cobalthex@live.com>
2017-02-07 17:26:43 -0800

    Working on groups

---
+++ DyingAndMore/Editor/Editor.Groups.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Graphics;
+using Takai.Input;
+using Takai.Graphics;
+
+namespace DyingAndMore.Editor
+{
+    partial class Editor : Takai.GameState.GameState
+    {
+        //todo: on mode load, set all existing groups to purple colored
+
+        void UpdateGroupsMode(GameTime Time)
+        {
+
+        }
+
+        void DrawGroupsMode()
+        {
+
+        }
+    }
+}
---
+++ Takai/Game/Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Takai.Game
+{
+    /// <summary>
+    /// A group of entities. Can be used for things like triggers or conditional spawns
+    /// </summary>
+    public class Group
+    {
+        /// <summary>
+        /// A unique name for this group
+        /// </summary>
+        public string Name { get; set; }
+
+        public List<Entity> Entities { get; set; }
+    }
+}
