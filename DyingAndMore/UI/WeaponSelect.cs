c7f5065222f674e5f8bd283de3f0b510d413105b
cobalthex <cobalthex@live.com>
2018-10-19 17:18:52 -0700

    Modernizing ItemList

---
+++ DyingAndMore/UI/WeaponSelect.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using DyingAndMore.Game.Weapons;
+
+namespace DyingAndMore.UI
+{
+    public class WeaponSelect : Takai.UI.DropdownSelect<WeaponClass>
+    {
+    }
+}
