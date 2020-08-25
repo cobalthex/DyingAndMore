adff8b929aac4bb91e20fa7bb286aa2aced116e3
CobaltHex <none@none>
2020-08-25 01:20:19 -0700

    Fixed letter casing

--- Tools/FixCases.ps1
+++ Tools/FixCases.ps1
@@ -6,15 +6,26 @@ Param(
 
 Push-Location $Path
 
+$queue = Get-ChildItem -Recurse -Path * -File -Include '*.tk'
 $allFiles = @{}
-Get-ChildItem -Recurse -Path * -File -Include '*.tk' | % {
+Get-ChildItem -Recurse -Path * -File | % {
     $rel = (Resolve-Path -Relative $_)
     $rel = $rel.Substring(2).Replace('\', '/')
-    $allFiles[$rel] = $rel
+    $allFiles[$_] = $rel
 }
 
-Pop-Location $Path
+Pop-Location
 
+# not efficient
+$queue | ForEach-Object -Parallel {
+    foreach ($kv in $allFiles.GetEnumerator()) {
+        $p = $kv.Value
+        $lp = './' + $kv.Key.Name
+        # -raw is not ideal, but out-file -NoNewLine will remove all newlines if done per-line
+        $s = (Get-Content -LiteralPath $_ -raw) -replace "@([`"'])$p[`"']","@`$1$p`$1"
+        $s = $s -replace "@([`"'])$lp[`"']","@`$1$lp`$1"
+        $s | out-file -Encoding utf8 -NoNewLine $_
+    }
+}
 
-
-$allFiles
+#$allFiles
